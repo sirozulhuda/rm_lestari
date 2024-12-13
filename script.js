a335696014ab8b6bff38a4c0409513251ec6affc
Sirozul Huda <sirozulhuda18@gmail.com>
2024-12-13 13:15:10 +0700

    menambahkan source code project

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+  window.scrollTo(0, 0); // Scroll ke posisi x=0, y=0
+};
+
+const navIcon = document.getElementById("nav-icon");
+const navMenu = document.querySelector(".nav-menu");
+
+navIcon.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  const icon = navIcon.querySelector("i");
+
+  // Ganti icon hamburger ke icon close
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  }
+});
